b8fcb35b4a9174af767a61df96d9744addd0c5b0
Jakub <jakub.bchyla@gmail.com>
2022-03-06 08:55:08 +0100

    Code Wars

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,16 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Solution {
     public static int century(int number) {
-        //}
-        return 345234;
+        int num = 0;
+        if(number % 100 != 0){
+            num = (number / 100) + 1;
+        } else{
+            num = (number / 100);
+        }
+        return num;
     }
 }
