30c61fed98a23ff7ab8b2e1a518be6d46db1f016
Jakub <jakub.bchyla@gmail.com>
2022-03-11 06:56:14 +0100

    Code Wars

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -2,7 +2,30 @@ public class Sum
 {
     public int GetSum(int a, int b)
     {
-        //Good luck!
-        return 0;
+        int result = 0;
+        int min, max = 0;
+        int c = 0;
+
+        if(a>b){
+            min =b;
+            max =a;
+        }else {
+            min =a;
+            max=b;
+        } // min max
+        c = (Math.abs(max-min))+1;
+        System.out.println("amount of digits: " + c);
+        int[] array = new int[c];
+
+        for(int i = 0;i<c;i++){
+            array[i] =min++;
+        }
+        for (int i = 0 ; i <c; i++){
+            System.out.print(array[i]+ " ");
+        }
+        for (int i : array){
+            result += i;
+        }
+        return result;
     }
 }
