cdb2f72ffdafa117108c63a4c1dce47c469e96c8
Jakub <jakub.bchyla@gmail.com>
2022-03-15 16:26:27 +0100

    Code Wars

--- src/main/java/Scramblies.java
+++ src/main/java/Scramblies.java
@@ -1,15 +1,27 @@
+import java.util.ArrayList;
+
 public class Scramblies {
 
     public static boolean scramble(String str1, String str2) {
         boolean result = false;
-        for(int i = 0; i <str1.length(); i++){
+        ArrayList<String> arraylist1 = new ArrayList<>();
+        ArrayList<String> arraylist2 = new ArrayList<>();
 
-
-            if(str2.contains(Character.toString(str1.charAt(i)))){
+        for(int i = 0 ; i<str2.length() ; i++){
+            arraylist1.add(Character.toString(str2.charAt(i)));
+        }
+        for(int i = 0 ; i<str1.length() ; i++){
+            arraylist2.add(Character.toString(str1.charAt(i)));
+        }
+        for(int i = 0; i <arraylist1.size(); i++){
+            if(arraylist2.contains(arraylist1.get(i))){
+               //System.out.println(arraylist1.get(i));
                 result = true;
-                str2 = str2.replace(Character.toString(str1.charAt(i))," ");
-                //System.out.println(str1 + "  -  " + str2);
-
+                int index = arraylist2.indexOf(arraylist1.get(i));
+                //System.out.println(arraylist2.get(i));
+                //System.out.println(index);
+                arraylist2.set(index, "-"); //replace   indexoff
+                //System.out.println(arraylist2);
             }else {
                 result = false;
                 break;
