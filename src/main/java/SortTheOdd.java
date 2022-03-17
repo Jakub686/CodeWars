93cf9dd7f0e0b4f5237d54c875f13c200674710a
Jakub <jakub.bchyla@gmail.com>
2022-03-17 20:19:20 +0100

    Code Wars

--- src/main/java/SortTheOdd.java
+++ src/main/java/SortTheOdd.java
@@ -1,24 +1,15 @@
 public class SortTheOdd {
     public static int[] sortArray(int[] array) {
-
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 int tmp = 0;
-                if(array[i]%2!=0){
-
-                    if (array[i] > array[j] & array[j]%2!=0 ) {
-                        tmp = array[i];
-                        array[i] = array[j];
-                        array[j] = tmp;
-                    }
+                if (array[i] % 2 != 0 & array[i] > array[j] & array[j] % 2 != 0) {
+                    tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
                 }
             }
         }
-
         return array;
     }
 }
-
-/*
-Array a -> oddArray, evenArray -> sort oddArray
- */
