63cfcc6e3b614c9cf730450dc85f8ad29e5fafb3
Jakub <jakub.bchyla@gmail.com>
2022-03-20 07:42:54 +0100

    Code Wars

--- src/main/java/MiddleExample.java
+++ src/main/java/MiddleExample.java
@@ -1,6 +1,15 @@
 public class MiddleExample {
     public static String getMiddle(String word) {
-        String result = "a";
+        String result = "";
+        StringBuilder sb = new StringBuilder(word);
+        int sbLength = sb.length();
+        int sbMiddle = sb.length()/2;
+        if(sbLength%2==0){
+            sb = sb.delete(0,sbMiddle-1).delete(2,sb.length());
+        }else {
+            sb = sb.delete(0,sbMiddle).delete(1,sb.length());
+        }
+        result = sb.toString();
         return result;
     }
 }
---
+++ src/test/java/MergedStringCheckerTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MergedStringCheckerTest {
+
+    @Test
+    public void normalHappyFlow() {
+        assertTrue("codewars can be created from code and wars", MergedStringChecker.isMerge("codewars", "code", "wars"));
+        assertTrue("codewars can be created from cdwr and oeas", MergedStringChecker.isMerge("codewars", "cdwr", "oeas"));
+        assertFalse("Codewars are not codwars", MergedStringChecker.isMerge("codewars", "cod", "wars"));
+    }
+
+}
