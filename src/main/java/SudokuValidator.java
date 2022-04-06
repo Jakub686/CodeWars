e1cf7befa9de627fbfa9f20b9dd4a6f16044973b
Jakub <jakub.bchyla@gmail.com>
2022-04-06 15:25:49 +0200

    Code Wars

--- src/main/java/SudokuValidator.java
+++ src/main/java/SudokuValidator.java
@@ -1,12 +1,16 @@
 public class SudokuValidator {
     public static boolean check(int[][] sudoku) {
+        int[] tempArray = new int[9];
         boolean result = true;
-        for(int i = 0; i <sudoku[0].length; i++){
-            if(validator(sudoku[i])==true){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                tempArray[j] = sudoku[i][j];
+            }
+            if (validator(tempArray) == true) {
                 result = true;
-                System.out.println("true");
-            }else {
+            } else {
                 result = false;
+                break;
             }
         }
         return result;
@@ -18,8 +22,7 @@ public class SudokuValidator {
         for (int i = 0; i < sudoku.length; i++) {
             sudokuString += String.valueOf(sudoku[i]);
         }
-
-        if (sudokuString.contains("1") && sudokuString.contains("2") && sudokuString.contains("3") && sudokuString.contains("4") && sudokuString.contains("5") && sudokuString.contains("6") && sudokuString.contains("7") && sudokuString.contains("8") && sudokuString.contains("9") && !sudokuString.contains("0")) {
+        if (sudokuString.contains("1") & sudokuString.contains("2") & sudokuString.contains("3") & sudokuString.contains("4") & sudokuString.contains("5") & sudokuString.contains("6") & sudokuString.contains("7") & sudokuString.contains("8") & sudokuString.contains("9") & !sudokuString.contains("0")) {
             result = true;
         } else {
             result = false;
