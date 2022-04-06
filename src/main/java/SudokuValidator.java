7473040d87b38c5c7a06bf68a638939d1144f540
Jakub <jakub.bchyla@gmail.com>
2022-04-06 16:32:10 +0200

    Code Wars

--- src/main/java/SudokuValidator.java
+++ src/main/java/SudokuValidator.java
@@ -1,18 +1,60 @@
 public class SudokuValidator {
     public static boolean check(int[][] sudoku) {
-        int[] tempArray = new int[9];
+        for(int i=0; i< sudoku.length; i++) {
+            for (int j = 0; j < sudoku[i].length; j++) {
+                System.out.print(sudoku[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+
         boolean result = true;
         for (int i = 0; i < 9; i++) {
+            int[] tempArray = new int[9]; // validator row
             for (int j = 0; j < 9; j++) {
                 tempArray[j] = sudoku[i][j];
             }
             if (validator(tempArray) == true) {
                 result = true;
+
             } else {
                 result = false;
                 break;
             }
         }
+
+        for (int i = 0; i < 9; i++) {
+            int[] tempArray = new int[9]; // validator column
+            for (int j = 0; j < 9; j++) {
+                tempArray[j] = sudoku[j][i];
+            }
+            if (validator(tempArray) == true) {
+                result = true;
+
+            } else {
+                result = false;
+                break;
+            }
+        }
+
+        //for(int k = 0 ; k<9 ; k++) {
+            for (int i = 0; i < 9; i++) {
+                int[] tempArray = new int[9]; // validator box 1/9
+                for (int j = 0; j < 9; j++) {
+                    tempArray[j] = sudoku[j][i];
+                }
+                if (validator(tempArray) == true) {
+                    result = true;
+
+                } else {
+                    result = false;
+                    break;
+                }
+            }
+        //}
+
+
+
         return result;
     }
 
